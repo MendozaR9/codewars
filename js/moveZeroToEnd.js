8ac19bb442a7b37cb141e9bb20bca7ca701647d2
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-07-04 23:27:02 -0500

    created a better function that uses filter to get the zeros at the end

--- js/moveZeroToEnd.js
+++ js/moveZeroToEnd.js
@@ -2,18 +2,26 @@ function moveZeros(arr){
     let noZero = []
     let zero =[]
     for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i])
-        console.log(arr[i] !== 0)
         if (arr[i] !== 0){
             noZero.push(arr[i])
         } else if (arr[i] === 0) {
             zero.push(arr[i])
         }
     }
-  let zeroAtEnd = noZero.concat(zero)
-    return zeroAtEnd
+    return noZero.concat(zero)
 
 }
 
+function betterMoveZeros(arr){
+    let noZeros = arr.filter(function (num){
+        return num !== 0;
+    })
+    let zeros = arr.filter(function (num){
+        return num  === 0;
+    })
+    return noZeros.concat(zeros)
+}
+
+
 let arry = [[],{},1,2,0,1,0,1,0,3,0,1]
-console.log(moveZeros(arry))
+console.log(betterMoveZeros(arry))
