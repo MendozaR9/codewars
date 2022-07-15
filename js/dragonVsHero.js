7653dc1d38b79557b16ffa7d907e4b931899eee4
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-07-15 15:32:01 -0500

    created a function that coverts a number to a negative

---
+++ js/dragonVsHero.js
@@ -0,0 +1,6 @@
+function hero(bullet, dragon){
+    let total = bullet / dragon
+    return total >= 2;
+}
+
+console.log(hero(10, 5))
---
+++ js/makeNegative.js
@@ -0,0 +1,7 @@
+function negative(num){
+    if (num > 0){
+        return -num
+    }else return num
+}
+
+console.log(negative(47))
