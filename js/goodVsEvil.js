5e11304d416b192d4a917ee16c152558acb0753e
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-09-27 20:36:41 -0500

    finish codewars goodVsEvil

--- js/goodVsEvil.js
+++ js/goodVsEvil.js
@@ -1,5 +1,6 @@
 function goodVsEvil(good,evil){
     let goodSolider =good.split(" ")
+    let evilSolider = evil.split(" ")
     let goodWorth =0
     let evilWorth =0
     let goodCount = [1,2,3,3,4,10]
@@ -7,20 +8,20 @@ function goodVsEvil(good,evil){
     for (let i = 0; i < goodSolider.length; i++) {
        goodWorth += goodSolider[i]*goodCount[i]
     }
-
-    console.log(goodWorth)
-
-
-// evil
-//     Orcs: 1
-//     Men: 2
-//     Wargs: 2
-//     Goblins: 2
-//     Uruk Hai: 3
-//     Trolls: 5
-//     Wizards: 10
+    for (let i = 0; i < evilSolider.length; i++) {
+        evilWorth += evilSolider[i]* evilCount[i]
+    }
+    return whoWins(goodWorth, evilWorth)
 
 }
+    function whoWins(good, evil){
+       if(good > evil){
+           return "Battle Result: Good triumphs over Evil"
+       }else if (good <evil){
+           return "Battle Result: Evil eradicates all trace of Good"
+       }else return "Battle Result: No victor on this battle field"
+    }
+
 let good1 = "1 1 1 1 1 1"
 let evil1 = "1 1 1 1 1 1 1"
-goodVsEvil(good1, evil1)
+console.log(goodVsEvil(good1, evil1));
