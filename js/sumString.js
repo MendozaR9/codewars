01731ef5a6bcc2c22dadd814edeebc3822ef8789
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-10-26 21:31:17 -0500

    codewars

--- js/sumString.js
+++ js/sumString.js
@@ -1,4 +1,5 @@
 function sumString(a,b){
+
     let sum;
     if (!isNaN(parseFloat(a)) && !isNaN(parseFloat(b))){
         sum = parseFloat(a)+parseFloat(b)
@@ -7,8 +8,15 @@ function sumString(a,b){
     } else if (!isNaN(parseFloat(b)) && isNaN(parseFloat(a))){
         sum= parseFloat(b)
     }
-    return sum.toString();
+return sum.toString()
+    // return  sum.toLocaleString('fullwide', {useGrouping: false})
 }
+
+
+
+let test1 ="712569312664357328695151392"
+let test2 ="8100824045303269669937"
 
 console.log(sumString("123", "456"))
 console.log(sumString('', 5))
+console.log(sumString(test1,test2))
